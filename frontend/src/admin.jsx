0697724bc872cdc9e313e616a73f77d6607b658f
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2026-06-24 23:54:03 +0530

    frontend basic functionalities

--- frontend/src/admin.jsx
+++ frontend/src/admin.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Admin() {
-  return (
-    <div>I am Admin 1</div>
-  )
+    const [formData, setFormData] = useState({ company: "", email: "", password: "" });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formData.company !== "" && formData.email !== "" && formData.password !== "") {
+            console.log("Form submitted");
+        }
+        else {
+            alert("Please fill all details")
+        }
+
+    };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="company" placeholder="Enter company name" value={formData.company} onChange={handleChange} />
+                <input type="text" name="email" placeholder="Enter your email" value={formData.email} onChange={handleChange} />
+                <input type="text" name="password" placeholder="Enter your password" value={formData.password} onChange={handleChange} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
 }
 
-export default Admin
+export default Admin;
--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 function Home() {
     const navigate=useNavigate();
-    function handleClick(){
+    function handleAdminClick(){
         navigate("/admin")
     }
-    function handleuserClick(){
+    function handleUserClick(){
         navigate("/user")
     }
   return (
     <div>
         <h1>Welcome to HireTrack</h1>
         <h2>Who are you?</h2>
-        <button onClick={handleClick}>Admin</button>
-        <button onClick={handleuserClick}>User</button>
+        <button onClick={handleAdminClick}>Admin</button>
+        <button onClick={handleUserClick}>User</button>
     </div>
   )
 }
