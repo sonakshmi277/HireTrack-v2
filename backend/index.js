ceab72dc4350defb697c0893b7933c6cd88ff52a
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2026-07-30 23:07:12 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -204,7 +204,6 @@ app.patch("/editJob/:id", auth, async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 })
-
 app.get("/jobCount", auth, async (req, res) => {
   try {
     const ct = await Job.countDocuments();
